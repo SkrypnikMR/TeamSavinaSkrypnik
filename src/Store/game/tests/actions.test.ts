81d3a865fa0bad57e9a2f15d39667965425351b1
SkrypnikMR <Skripnikmrw@gmail.com>
2021-07-23 10:39:01 +0300

    aded all tests in new game actions

--- src/Store/game/tests/actions.test.ts
+++ src/Store/game/tests/actions.test.ts
@@ -61,4 +61,137 @@ describe('game actions', () => {
             expect(actions.playWithBot(payload)).toEqual({ type: actionTypes.PLAY_WITH_BOT, payload });
         });
     });
+    describe('actions.createRoom ', () => {
+        it('toBe defined', () => {
+            expect(actions.createRoom).toBeDefined();
+        });
+        it('toBe function', () => {
+            expect(typeof actions.createRoom).toBe('function');
+        });
+        it('should return value', () => {
+            const uuid = '21863723621-sdaosd2153123';
+            expect(actions.createRoom(uuid))
+                .toEqual({ type: actionTypes.CREATE_ROOM, payload: uuid });
+        });
+    });
+    describe('actions.setActualRoom  ', () => {
+        it('toBe defined', () => {
+            expect(actions.setActualRoom).toBeDefined();
+        });
+        it('toBe function', () => {
+            expect(typeof actions.setActualRoom).toBe('function');
+        });
+        it('should return value', () => {
+            const actualRoom = {
+                id: '213821732173t213',
+                guestLogin: 'Maxik',
+                creatorLogin: 'NeMAxik',
+                startTime: Date.now(),
+            };
+            expect(actions.setActualRoom(actualRoom))
+                .toEqual({ type: actionTypes.SET_ACTUAL_ROOM, payload: actualRoom });
+        });
+    });
+    describe('actions.subscribeRoom', () => {
+        it('toBe defined', () => {
+            expect(actions.subscribeRoom).toBeDefined();
+        });
+        it('toBe function', () => {
+            expect(typeof actions.subscribeRoom).toBe('function');
+        });
+        it('should return value', () => {
+            const uuid = 'asdusaytdgsadyhgu213861287362183721';
+            expect(actions.subscribeRoom(uuid))
+                .toEqual({ type: actionTypes.SUBSCRIBE_ROOM, payload: uuid });
+        });
+    });
+    describe('actions.deleteRoom ', () => {
+        it('toBe defined', () => {
+            expect(actions.deleteRoom).toBeDefined();
+        });
+        it('toBe function', () => {
+            expect(typeof actions.deleteRoom).toBe('function');
+        });
+        it('should return value', () => {
+            expect(actions.deleteRoom())
+                .toEqual({ type: actionTypes.DELETE_ROOM });
+        });
+    });
+    describe('actions.cleanOldGame  ', () => {
+        it('toBe defined', () => {
+            expect(actions.cleanOldGame).toBeDefined();
+        });
+        it('toBe function', () => {
+            expect(typeof actions.cleanOldGame).toBe('function');
+        });
+        it('should return value', () => {
+            expect(actions.cleanOldGame())
+                .toEqual({ type: actionTypes.CLEAN_OLD_GAME });
+        });
+    });
+    describe('actions.getStepOrder', () => {
+        it('toBe defined', () => {
+            expect(actions.getStepOrder).toBeDefined();
+        });
+        it('toBe function', () => {
+            expect(typeof actions.getStepOrder).toBe('function');
+        });
+        it('should return value', () => {
+            const stepOrderTestPayload = { uuid: '213123213213', gameType: 'Chekers' };
+            expect(actions.getStepOrder(stepOrderTestPayload))
+                .toEqual({ type: actionTypes.GET_STEP_ORDER, payload: stepOrderTestPayload });
+        });
+    });
+    describe('actions.setStepOrder ', () => {
+        it('toBe defined', () => {
+            expect(actions.setStepOrder).toBeDefined();
+        });
+        it('toBe function', () => {
+            expect(typeof actions.setStepOrder).toBe('function');
+        });
+        it('should return value', () => {
+            const stepOrder = 'YOU';
+            expect(actions.setStepOrder(stepOrder))
+                .toEqual({ type: actionTypes.SET_STEP_ORDER, payload: stepOrder });
+        });
+    });
+    describe('actions.doTicStep ', () => {
+        it('toBe defined', () => {
+            expect(actions.setStepOrder).toBeDefined();
+        });
+        it('toBe function', () => {
+            expect(typeof actions.doTicStep).toBe('function');
+        });
+        it('should return value', () => {
+            const step = '7';
+            expect(actions.doTicStep(step))
+                .toEqual({ type: actionTypes.DO_TIC_STEP, payload: step });
+        });
+    });
+    describe('actions.setStepHistory ', () => {
+        it('toBe defined', () => {
+            expect(actions.setStepHistory).toBeDefined();
+        });
+        it('toBe function', () => {
+            expect(typeof actions.setStepHistory).toBe('function');
+        });
+        it('should return value', () => {
+            const stepHistory = [];
+            expect(actions.setStepHistory(stepHistory))
+                .toEqual({ type: actionTypes.SET_STEP_HISTORY, payload: stepHistory });
+        });
+    });
+    describe('actions.setWinner ', () => {
+        it('toBe defined', () => {
+            expect(actions.setWinner).toBeDefined();
+        });
+        it('toBe function', () => {
+            expect(typeof actions.setWinner).toBe('function');
+        });
+        it('should return value', () => {
+            const winner = 'ME';
+            expect(actions.setWinner(winner))
+                .toEqual({ type: actionTypes.SET_WINNER, payload: winner });
+        });
+    });
 });
