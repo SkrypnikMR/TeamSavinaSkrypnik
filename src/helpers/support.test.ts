4a09a2da2ecaa6cfba84c2f038862ed8e9929be4
SkrypnikMR <Skripnikmrw@gmail.com>
2021-07-25 13:50:23 +0300

    added new test on support func and repair errorCatcher

--- src/helpers/support.test.ts
+++ src/helpers/support.test.ts
@@ -1,12 +1,18 @@
 import { NotificationManager } from 'react-notifications';
+import moment from 'moment';
 import { support } from './support';
 import { store } from '../index';
 import { doBotStepTic, gameEvent } from '../store/game/actions';
 
-const { setTokenInCookie,
+const {
+    subBot,
+    subGame,
+    errorCatcher,
+    getPrettyDate,
+    setTokenInCookie,
     getTokenFromCookie,
     deleteTokenFromCookie,
-    errorCatcher, subGame, subBot } = support;
+     } = support;
 
 jest.mock('react-notifications', () => ({
     NotificationManager: { error: jest.fn() },
@@ -65,6 +71,13 @@ describe('support', () => {
         it('should be function', () => {
             expect(typeof errorCatcher).toBe('function');
         });
+        it('should return 1 and not call NotificationManager', () => {
+            const errorMessage = 'Not your turn Bot';
+            const messageBody = JSON.stringify({ body: errorMessage });
+            const message = { body: messageBody };
+            expect(errorCatcher(message)).toBe(1);
+             expect(NotificationManager.error).not.toHaveBeenCalled();
+        });
         it('should call NotificationManager.error with right arguments', () => {
             const errorMessage = 'error message';
             const messageBody = JSON.stringify({ body: errorMessage });
@@ -97,6 +110,19 @@ describe('support', () => {
             const payload = '4';
             subBot({ body: payload });
             expect(store.dispatch).toHaveBeenCalledWith(doBotStepTic(payload));
+        });
+    });
+    describe('getPrettyDate', () => {
+        it('should be defined', () => {
+            expect(getPrettyDate).toBeDefined();
+        });
+        it('should be function', () => {
+            expect(typeof getPrettyDate).toBe('function');
+        });
+        it('should call moment with testTimeStamp and return in format like in func', () => {
+            const testTimeStamp = 123123214214;
+            expect(getPrettyDate(testTimeStamp))
+                .toBe(moment(testTimeStamp).format('L h:mm:ss'));
         });
     });
 });
